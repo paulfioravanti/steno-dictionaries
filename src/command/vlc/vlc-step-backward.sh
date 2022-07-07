f114ff7b58a0c197496f530caf6e1acdb60d8013
Paul Fioravanti <paul.fioravanti@gmail.com>
2022-07-07 10:32:17 +1000

    Add some VLC shell scripts for use with Stream Deck Pedal

---
+++ src/command/vlc/vlc-step-backward.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script was created for use with an Elgato Stream Deck Pedal.
+# Assign a System > Open action to a pedal and reference this script
+# in the "App/File" field.
+
+bash -ci 'osascript $STENO_DICTIONARIES/src/command/vlc/vlc-step-backward.scpt'
